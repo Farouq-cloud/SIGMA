3baeeb8757d52ea71bbe5ae631b08a827c98c9fd
fersubair40 <olajnrsubair@gmail.com>
2019-09-19 23:26:23 +0100

    authentication system

--- auth.py
+++ auth.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, url_for, redirect, request
+from flask import Blueprint, render_template, url_for, redirect, request,flash
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user
 from models import User
 from main import db
 
@@ -8,6 +9,28 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login')
 def login():
     return render_template("login.html")
+
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('A user with that username  does not exist')
+        return render_template('login.html')
+    is_correct_password = check_password_hash(user.password, password)
+    print(is_correct_password)
+    if not is_correct_password:
+        flash('Wrong Credentials')
+        return render_template('login.html')
+    flash('Log in successful')
+    return redirect('/')
+
+
+
+
 
 
 @auth.route('/')
@@ -21,19 +44,28 @@ def signup_post():
     password = request.form.get('password')
     confirm_password = request.form.get('confirm_password')
 
-    user = User.query.filter_by(email=email).first()
+    if username is None or password is None or email is None or confirm_password is None:
+            flash('username, email, password are required')
+            return render_template('index.html')
+    if ' ' in username:
+            flash('Username should not contain spaces')
+            return render_template('index.html')
+    if password != confirm_password:
+            flash("Passwords do not match")
+            return render_template('index.html')
+    else:
+            user = User.query.filter_by(email=email).first()
+            if user is not None:
+                flash('A user with that email already exists')
+                return render_template('index.html')
+            user = User.query.filter_by(email=email).first()
+            if user is not None:
+                flash('A user with that email already exists')
+                return render_template('index.html')
 
-    if not user:
-        if password == confirm_password:
-            new_user = User(email=email, username=username, password=password, confirm_password=confirm_password)
+            new_user = User(username=username, email=email, password=generate_password_hash(password, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            return redirect(url_for('auth.login'))
 
-    return redirect(url_for('auth.login'))
-
-
-
-
-@auth.route('/logout')
-def logout():
-    return 'Logout'
+    return render_template('signup.html')
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 
 db = SQLAlchemy()
 
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'wejfnfewiufwifwefw'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
     
 db.init_app(app)
 
